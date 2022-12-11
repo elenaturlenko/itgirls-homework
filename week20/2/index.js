ad09f19edfbc059e4cf42fbd7e2a76cc3b117ad5
elenaturlenko <turlenkoelena@gmail.com>
2022-12-11 18:55:12 +0500

    ДЗ недели 20

--- week20/2/index.js
+++ week20/2/index.js
@@ -1,16 +1,12 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  getGalaxy('2022-12-08');
-});
-
-function getGalaxy(date) {
-  fetch('https://api.nasa.gov/planetary/apod?api_key=1PmGMa75hCmBHTCNfjxabuhMbQHaes4kVJd8Mr3h')
-.then(response => response.json())
-.then(galaxy => {
-  console.log(galaxy );
+  
+fetch('https://api.nasa.gov/planetary/apod?api_key=1PmGMa75hCmBHTCNfjxabuhMbQHaes4kVJd8Mr3h')
+  .then(response => response.json())
+  .then(galaxy => {
     document.querySelector('.title').innerHTML = galaxy.title;
     document.querySelector('.date').innerHTML = galaxy.date;
     document.querySelector('.description').innerHTML = galaxy.explanation;
     document.querySelector('.img').src = galaxy.hdurl;
 })
-.catch(error => console.log(error));
-}
+
+});
