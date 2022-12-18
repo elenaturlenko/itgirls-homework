ffd81db33b4e4d470cef8300ba49ea0b96de3be3
elenaturlenko <turlenkoelena@gmail.com>
2022-12-18 12:10:34 +0500

    Дз недели 21

--- week21/1/index.js
+++ week21/1/index.js
@@ -1,15 +1,16 @@
 function getGif() {
 
-  let searchValue=document.querySelector(".search").value;
-  let api_key="IxdZXMElSNTw3KAXgkCjqI9YW1uqaYsd";
+  let searchValue = document.querySelector(".search").value;
+  let api_key = "IxdZXMElSNTw3KAXgkCjqI9YW1uqaYsd";
 
   document.querySelector(".container").innerHTML = "";
 
   fetch(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${searchValue}&limit=5&offset=0&rating=g&lang=ru`)
-  .then(response=>response.json())
+  .then(response => response.json())
   .then(gif => {
-      for (let i = 0; i < gif.length ; i++) {
-          let img = gif[i];
+    console.log(gif);
+      for (let i = 0; i < gif.data.length ; i++) {
+          let img = gif.data[i].images.original.url;;
           document.querySelector(".container").innerHTML += `<img src="${img}" alt="gif">`;
       }
   })
